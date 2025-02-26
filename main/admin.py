858b13249ed8d49f9f3e47381046aa99140cd91e
Enimarques Filho <enimarkespessoa@gmail.com>
2025-02-26 15:23:40 -0300

    Mudanças e ajustes nos models

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
+from django.utils.timezone import now
+from .models import Pessoa , Setor , Usuario, Veiculo
 
-from .models import Visitante , Setor , Usuario, Veiculo
-
-class VisitanteAdmin(admin.ModelAdmin):
+class PessoaAdmin(admin.ModelAdmin):
   list_display = ('nome_completo','CPF','data_nascimento')  #as colunas que vao ser visiveis
   search_fields = ('nome_completo', 'CPF')        #os campos de pesquisa
   
@@ -16,11 +16,13 @@ class UsuarioAdmin(admin.ModelAdmin):
   search_fields = ('usuario',)
 
 class VeiculoAdmin(admin.ModelAdmin):
-  list_display = ('placa', 'horario_entrada')
-  search_fields = ('placa', 'horario_entrada')
-  list_filter = ('placa',)
+  list_display = ('placa', 'responsavel', 'horario_entrada', 'horario_saida')
+  actions = ['registrar_saida']
+  search_fields = ('placa', 'horario_entrada', 'responsavel')
+  list_filter = ('placa','horario_entrada', 'horario_saida')
 
-admin.site.register(Visitante, VisitanteAdmin)
+
+admin.site.register(Pessoa, PessoaAdmin)
 admin.site.register(Setor, SetorAdmin)
 admin.site.register(Veiculo, VeiculoAdmin)
 admin.site.register(Usuario, UsuarioAdmin)
--- main/models.py
+++ main/models.py
@@ -1,17 +1,62 @@
+#espaço para as importaçoes do projeto
+
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
+from django.utils.timezone import get_current_timezone
 
 #CRIANDO TABELA VISITANTES E SETORES
-class Visitante(models.Model):
-  nome_completo = models.CharField(max_length=70)
-  nome_social = models.CharField(max_length=70, blank=True, null=True)
-  data_nascimento = models.DateField(blank=True , null=True)  #FORMATO ERRADO, CORRIGIR
-  CPF = models.CharField(max_length=14, unique=True)      #depois aprimorar importando o validationerror e validate_docbr import cpf
-  foto = models.ImageField(upload_to='fotos_visitante/')
-  data_registro = models.DateTimeField(auto_now_add=True)  #REGISTRA A DATA DE REGISTRO DO USUARIO
-  
-  def __str__(self):
+class Pessoa(models.Model):
+    ESTADOS_CHOICES = [
+        ('AC', 'Acre'),
+        ('AL', 'Alagoas'),
+        ('AP', 'Amapá'),
+        ('AM', 'Amazonas'),
+        ('BA', 'Bahia'),
+        ('CE', 'Ceará'),
+        ('DF', 'Distrito Federal'),
+        ('ES', 'Espírito Santo'),
+        ('GO', 'Goiás'),
+        ('MA', 'Maranhão'),
+        ('MT', 'Mato Grosso'),
+        ('MS', 'Mato Grosso do Sul'),
+        ('MG', 'Minas Gerais'),
+        ('PA', 'Pará'),
+        ('PB', 'Paraíba'),
+        ('PR', 'Paraná'),
+        ('PE', 'Pernambuco'),
+        ('PI', 'Piauí'),
+        ('RJ', 'Rio de Janeiro'),
+        ('RN', 'Rio Grande do Norte'),
+        ('RS', 'Rio Grande do Sul'),
+        ('RO', 'Rondônia'),
+        ('RR', 'Roraima'),
+        ('SC', 'Santa Catarina'),
+        ('SP', 'São Paulo'),
+        ('SE', 'Sergipe'),
+        ('TO', 'Tocantins'),
+    ]
+    nome_completo = models.CharField(max_length=70)
+    nome_social = models.CharField(max_length=70, blank=True, null=True)
+    data_nascimento = models.DateField(blank=True , null=True)  #FORMATO ERRADO, CORRIGIR
+    CPF = models.CharField(max_length=14, unique=True)      #depois aprimorar importando o validationerror e validate_docbr import cpf
+    telefone = models.CharField(max_length=20)
+    email = models.CharField(max_length=80, null=True , blank=True)
+    estado = models.CharField(max_length=2, choices=ESTADOS_CHOICES, null=True, blank=True)
+    cidade = models.CharField(max_length=20, null=True, blank=True)
+    foto = models.ImageField(upload_to='fotos_visitante/')
+    data_registro = models.DateTimeField(auto_now_add=True)  #REGISTRA A DATA DE REGISTRO DO USUARIO
+    
+    def __str__(self):
      return self.nome_completo
   
+'''class visitas(models.Model):
+    visitante = models.ForeignKey(Visitante, on_delete=models.CASCADE)
+    data_saida = models.DateTimeField(auto_now_add=True)
+    local_saida = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return f'{self.visitante.nome} - {self.local_saida}'''
+    
 class Setor(models.Model):
   nome = models.CharField(max_length=100, unique=True)
   localização = models.CharField(max_length=255)
@@ -23,8 +68,6 @@ class Setor(models.Model):
 
     
 # CRIAÇÃO DOS TIPOS DE USUÁRIOS 
-
-from django.contrib.auth.hashers import make_password, check_password
 
 class Usuario(models.Model):
     TIPOS_USUARIO = [
@@ -53,4 +96,9 @@ class Usuario(models.Model):
 
 class Veiculo(models.Model):
    placa = models.CharField(max_length=10, unique=True)
+   responsavel = models.CharField(max_length=80)
    horario_entrada = models.DateTimeField(auto_now_add=True)
+   horario_saida = models.DateTimeField(blank=True, null=True)
+
+   def __str__(self):
+      return f'{self.placa}'
