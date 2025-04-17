f3cfdc63f3a51d2354f6fa8d38daf1a6e3bc1668
TersanPlay <203478154+TersanPBS@users.noreply.github.com>
2025-04-17 19:07:14 -0300

    Removendo app veiculos_new que não estava sendo utilizado

--- apps/veiculos_new/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'apps.veiculos_new.apps.VeiculosNewConfig'
--- apps/veiculos_new/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class VeiculosNewConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.veiculos_new'
-    verbose_name = 'Veículos (Novo)'
