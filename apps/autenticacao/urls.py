2b9be9c466f8a98ebfdf5cee3f8db8b42ebfc8a4
Ariel Silva <tersandropbs@gmail.com>
2025-06-12 16:23:31 -0300

    atualização contesto visual nas paginas  e mini menu com avatar

--- apps/autenticacao/urls.py
+++ apps/autenticacao/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path
 from .views import login_sistema, logout_sistema
 from . import views_usuario, views_assessor
+from .user_views.user_views import perfil_usuario
 
 app_name = 'autenticacao'
 
@@ -14,6 +15,7 @@ urlpatterns = [
     path('usuarios/novo/', views_usuario.cadastro_usuario, name='criar_usuario'),
     path('usuarios/<int:pk>/editar/', views_usuario.editar_usuario, name='editar_usuario'),
     path('usuarios/<int:pk>/excluir/', views_usuario.excluir_usuario, name='excluir_usuario'),
+    path('perfil/', perfil_usuario, name='perfil_usuario'),
     
     # URLs de compatibilidade com o Django Admin
     re_path(r'^login/$', login_sistema),  # URL sem nome para compatibilidade com o admin
